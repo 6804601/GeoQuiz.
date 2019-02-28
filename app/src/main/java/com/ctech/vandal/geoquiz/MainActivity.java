ca7142bd08a4a0192dc139cdf28a2f5690912b56
6804601 <6806401@isd535.org>
2019-02-28 09:00:43 -0600

    Added buttons, resource ID's

--- app/src/main/java/com/ctech/vandal/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/vandal/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.ctech.vandal.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
