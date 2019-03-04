3ddd36e46a2864c953f2cd335c768f1bb70c4ffc
6804601 <6806401@isd535.org>
2019-03-04 09:04:42 -0600

    Painstakingly added the Toast while adjusting the gravity to make it all the way to the top while also being over the GeoQuiz bar.

--- app/src/main/java/com/ctech/vandal/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/vandal/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.ctech.vandal.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,17 +23,21 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
+                Toast toast = Toast.makeText(MainActivity.this,
                         R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.TOP, 0, 224);
+                        toast.show();
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
+                Toast toast = Toast.makeText(MainActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 224);
+                toast.show();
             }
         });
     }
